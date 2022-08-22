28ad4ab0edd8875aa02b59fe4f77ee1ec5393029
Bado Almaz <badoalmaz@Bados-MacBook-Pro.local>
2022-08-22 12:10:34 +0600

    context 3

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useProducts } from "../../contexts/ProductContextProvider";
 
 const ProductList = () => {
+  const { getProducts, products, pages } = useProducts();
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log(products);
+
   return <div>ProductList</div>;
 };
 
--- src/contexts/ProductContextProvider.js
+++ src/contexts/ProductContextProvider.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext, useReducer } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const productContext = createContext();
 export const useProducts = () => useContext(productContext);
@@ -33,6 +34,8 @@ const API = "https://backend-for-fs-makers.herokuapp.com/api/v1";
 const ProductContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
+  const navigate = useNavigate();
+
   async function getProducts() {
     try {
       const token = JSON.parse(localStorage.getItem("token"));
@@ -42,7 +45,7 @@ const ProductContextProvider = ({ children }) => {
           Authorization,
         },
       };
-      const res = await axios(`${API}​/products​/`, config);
+      const res = await axios(`${API}/products/`, config);
       console.log(res);
       dispatch({
         type: "GET_PRODUCTS",
@@ -53,7 +56,76 @@ const ProductContextProvider = ({ children }) => {
     }
   }
 
-  return <productContext.Provider>{children}</productContext.Provider>;
+  async function getCategories() {
+    try {
+      const token = JSON.parse(localStorage.getItem("token"));
+      const Authorization = `Bearer ${token.access}`;
+      const config = {
+        header: {
+          Authorization,
+        },
+      };
+
+      const res = await axios(`${API}/category/list/`, config);
+      console.log(res);
+      dispatch({
+        type: "GET_CATEGORIES",
+        payload: res.data.results,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function addProducts(newProduct) {
+    try {
+      const token = JSON.parse(localStorage.getItem("token"));
+      const Authorization = `Bearer ${token.access}`;
+      const config = {
+        header: {
+          Authorization,
+        },
+      };
+      const res = await axios.post(`${API}/products/`, newProduct, config);
+      console.log(res);
+      navigate("/products");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function deleteProduct(id) {
+    try {
+      const token = JSON.parse(localStorage.getItem("token"));
+      const Authorization = `Bearer ${token.access}`;
+      const config = {
+        header: {
+          Authorization,
+        },
+      };
+
+      await axios.delete(`${API}/products/${id}/`, config);
+      getProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <productContext.Provider
+      value={{
+        addProducts,
+        getProducts,
+        getCategories,
+        deleteProduct,
+        products: state.products,
+        pages: state.pages,
+        categories: state.categories,
+      }}
+    >
+      {children}
+    </productContext.Provider>
+  );
 };
 
 export default ProductContextProvider;
